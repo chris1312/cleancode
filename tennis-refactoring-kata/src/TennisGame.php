b8b40b2c80fd721bc403fff0b9be492fcfad9e06
kristijonas.bulzgis <kristijonas.bulzgis@visma.com>
2018-12-11 01:44:43 +0200

    Code katas

---
+++ tennis-refactoring-kata/src/TennisGame.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tennis;
+
+interface TennisGame
+{
+    /**
+     * @param  $playerName
+     * @return void
+     */
+    public function wonPoint($playerName);
+
+    /**
+     * @return string
+     */
+    public function getScore();
+}
